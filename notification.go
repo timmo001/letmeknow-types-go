17a43ceb5e7b7eb2be53abc136e9d15d6d5ec4e9
Aidan Timson <aidan@timmo.dev>
2024-07-15 14:46:25 +0100

    Move types from server

---
+++ notification.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Image struct {
+	URL string `json:"url"`
+}
+
+func (i Image) Display() string {
+	return fmt.Sprintf("  URL: %s", i.URL)
+}
+
+type Notification struct {
+	Type     string  `json:"type"` // "notification"
+	Title    *string `json:"title"`
+	Subtitle *string `json:"subtitle"`
+	Content  *string `json:"content"`
+	Image    *Image  `json:"image"`
+}
+
+func (n Notification) Display() string {
+	return fmt.Sprintf("Title: %s\nSubtitle: %s\nContent: %s\nImage:\n%s", *n.Title, *n.Subtitle, *n.Content, n.Image.Display())
+}
